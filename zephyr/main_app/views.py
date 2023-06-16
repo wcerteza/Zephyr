e4245bb4f955635e29d733e17ee32ed9da31ee71
Anthony Medina <anthony.john.medina@gmail.com>
2023-06-16 18:04:01 -0400

    stream page setup

--- zephyr/main_app/views.py
+++ zephyr/main_app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .forms import PostForm
 
 
 def stream(request):
     posts = Post.objects.all()
-    return render(request, "stream.html", {"posts": posts})
+    posts_form = PostForm()
+    return render(
+        request,
+        "stream.html",
+        {
+            "posts": posts,
+            "title": "Welcome To The Stream | Zephyr",
+            "posts_form": posts_form,
+        },
+    )
+
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ["title", "content"]
