8f933e954057526f6436d8c9848df4669e2fb5d4
Savino Lugo <Savinolugo@gmail.com>
2023-06-16 11:29:05 -0500

    Post model created

--- zephyr/main_app/models.py
+++ zephyr/main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=90)
+    content = models.TextField(max_length=160)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}({self.id}) at {created_at}"
