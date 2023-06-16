d77be692749e736f2047edc6c1aff0e63141b35e
Will Certeza <wcerteza@gmail.com>
2023-06-16 14:35:23 -0500

    complete models?

--- zephyr/main_app/admin.py
+++ zephyr/main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, Comment, Attachment
+from .models import Post, Comment, Attachment, Like
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Attachment)
+admin.site.register(Like)
