766947fca8fe2a3b76b1c36dbf2443dc662a0d23
Will Certeza <wcerteza@gmail.com>
2023-06-16 13:26:50 -0500

    Comment and attachment model created and working

--- zephyr/main_app/models.py
+++ zephyr/main_app/models.py
@@ -3,8 +3,17 @@ from django.urls import reverse
 from datetime import date
 
 
+class Comment(models.Model):
+    content = models.TextField(max_length=160)
+    created_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"added to {self.post}({self.id}) at {self.created_at}"
+
+
 class Post(models.Model):
-    title = models.CharField(max_length=90)
+    title = models.CharField(max_length=40)
     content = models.TextField(max_length=160)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -12,29 +21,20 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.title}({self.id}) at {self.created_at}"
 
-    # class Meta:
-    #     ordering = ["-date"]
+    class Meta:
+        ordering = ["-updated_at"]
 
 
-# class Comments(models.Model):
-#     title = models.CharField(max_length=90)
-#     content = models.TextField(max_length=160)
-#     created_at = models.DateTimeField(auto_now_add=True)
+class Attachment(models.Model):
+    url = models.CharField(max_length=200)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-#     def __str__(self):
-#         return f"{self.title}({self.id}) at {created_at}"
-
-
-# class Attachments(models.Model):
-#     url = models.CharField(max_length=200)
-#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-#     created_at = models.DateTimeField(auto_now_add=true)
-
-#     def __str__(self):
-#         return f"Attachment for post_id: {self.post_id} @{self.url}"
+    def __str__(self):
+        return f"Attachment for post_id: {self.post_id} @{self.url}"
 
 
-# class Likes(models.Model):
+# class Like(models.Model):
 #     title = models.CharField(max_length=90)
 #     content = models.TextField(max_length=160)
 #     created_at = models.DateTimeField(auto_now_add=True)
