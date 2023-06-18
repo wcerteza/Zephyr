d20e4fbcf1db211904a7eaecbcf2c58ce5755974
Kendal Chaney <lamarman808@gmail.com>
2023-06-17 23:13:56 -0400

    BUTTONS STYLE ON PUSH, CUH

--- zephyr/zephyr/settings.py
+++ zephyr/zephyr/settings.py
@@ -10,15 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+# from pathlib import Path
 
-import environ
+# import environ
 
-environ.Env()
-environ.Env.read_env()
+# environ.Env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
