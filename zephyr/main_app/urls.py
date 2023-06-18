5316621baa16d20a0527500f6c196a3200d855a2
Will Certeza <wcerteza@gmail.com>
2023-06-17 21:02:43 -0500

    added base functionality to auth

--- zephyr/main_app/urls.py
+++ zephyr/main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.stream, name="stream"),
     path("create/", views.PostCreate.as_view(), name="post_create"),
+    path("accounts/signup/", views.signup, name="signup"),
 ]
