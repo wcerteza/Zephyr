57c771d70e562d8c64c564a1193b9fb1c6be5500
Kendal Chaney <lamarman808@gmail.com>
2023-06-18 17:14:23 -0400

    DELETE WERKS, BRUV

--- zephyr/main_app/urls.py
+++ zephyr/main_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("create/", views.PostCreate.as_view(), name="post_create"),
     path("accounts/signup/", views.signup, name="signup"),
     path("posts/<int:post_id>/", views.posts_detail, name="detail"),
+    path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
+    path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete')
 ]
