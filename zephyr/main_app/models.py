70b2f7ed7cdf00e229f0c6dded745c941453b64a
Anthony Medina <anthony.john.medina@gmail.com>
2023-06-21 18:43:21 -0400

    render post images

--- zephyr/main_app/models.py
+++ zephyr/main_app/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     liked = models.ManyToManyField(
-        User#, default=None, blank=True, related_name="liked_posts"
+        User  # , default=None, blank=True, related_name="liked_posts"
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
 
@@ -41,24 +41,9 @@ class Post(models.Model):
 class Attachment(models.Model):
     url = models.CharField(max_length=200)
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
-    comment = models.ForeignKey("Comment", on_delete=models.CASCADE)
+    # comment = models.ForeignKey("Comment", on_delete=models.CASCADE, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Attachment for post_id: {self.post_id} @{self.url}"
-
-
-# LIKE_CHOICES = (
-#     ("Like", "Like"),
-#     ("Unlike", "Unlike"),
-# )
-
-
-# class Like(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     post = models.ForeignKey("Post", on_delete=models.CASCADE)
-#     value = models.CharField(choices=LIKE_CHOICES, default="Like", max_length=10)
-
-#     def __str__(self):
-#         return f"{self.user} liked {self.post}"
